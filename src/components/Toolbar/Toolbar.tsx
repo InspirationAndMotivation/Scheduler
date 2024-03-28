bb130e98ef7879ec5e37eb51bf3ebecb665c87fe
Anastasia Pranovych <anastasiia.pranovych@smartbear.com>
2024-03-28 11:56:05 +0200

    SCD-2: Theme changing functionality

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Toolbar.scss';
+import logo from '../../media/calendar2.png';
 
 const Toolbar = () => {
+  const [isLightTheme, setIsLightTheme] = useState(
+    JSON.parse(localStorage.getItem('theme') as string)
+  );
+
+  useEffect(() => {
+    document.body.setAttribute('theme', isLightTheme ? 'light' : 'dark');
+    localStorage.setItem('theme', JSON.stringify(isLightTheme ? true : false));
+  }, [isLightTheme]);
+
   return (
     <>
-      <div>
-        <h1>Toolbar</h1>
+      <div className="SchedulerToolbar">
+        <div className="LogoSide">
+          <img src={logo} alt="Logo" className="ToolbarLogo" />
+          <h1 className="AppName">Scheduler</h1>
+        </div>
+        <div className="ButtonsSide">
+          <input
+            type="checkbox"
+            id="theme-toggle"
+            checked={isLightTheme}
+            readOnly
+          />
+          <label
+            htmlFor="theme-toggle"
+            className="ThemeToggle"
+            onClick={() => setIsLightTheme(!isLightTheme)}
+          ></label>
+        </div>
       </div>
     </>
   );
